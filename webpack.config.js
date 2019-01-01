7dc9f8d64ab3c2cc53014ffdad6c3d3448896743
Dalton Montee <daltonmontee@gmail.com>
2019-01-01 16:24:34 -0600

    Update to webpack 4 and update babel packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,54 @@
+"use strict"
+
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const postcssOptions = require('./postcss.config');
 
 module.exports = {
 	entry: {
-		bundle: './src/index.js',
+		bundle: "./src/index.js",
 		vendor: [
-			"react",
-			"react-dom",
-			"react-redux",
-			"react-router",
-			"react-router-dom",
-			"redux",
-			"redux-thunk",
+			"@babel/polyfill",
 		]
 	},
 	output: {
-		path: path.join(__dirname, 'dist'),
-		publicPath: '/',
+		path: path.join(__dirname, "dist"),
+		publicPath: "/",
 		filename: '[name].[hash].js'
 	},
 	module: {
-		loaders: [
+		rules: [
 			{
-				test: /\.(html)$/,
-				use: {
-					loader: 'html-loader',
+				test: /\.html$/,
+				use: [{
+					loader: "html-loader",
 					options: {}
-				}
+				}]
 			},
 			{
-				test: /.(js|jsx)$/,
+				test: /.(j|t)sx?$/,
 				exclude: /node_modules/,
-				loader: 'babel-loader'
+				loader: "babel-loader"
 			},
 			{
 				test: /\.css$/,
-				use: ExtractTextPlugin.extract({
-				    fallback: 'style-loader',
-				    use: [
-				           'css-loader',
-				           {
-				           		loader: 'postcss-loader',
-				                options: postcssOptions
-				           }
-				    ]
-				})
+				use: [
+					"style-loader",
+					"css-loader",
+					{
+							loader: "postcss-loader",
+						 options: postcssOptions
+					}
+			 	]
 			}
 		]
 	},
 	resolve: {
-		extensions: ['.js', '.jsx']
+		extensions: [".js", ".jsx", ".ts", ".tsx"]
 	},
 	plugins: [
-		new webpack.optimize.CommonsChunkPlugin({
-			name: 'vendor',
-			filename: 'vendor.js',
-			minChunks: Infinity
-		}),
-		new HtmlWebpackPlugin({
-			template: 'index.html'
-		}),
-		new ExtractTextPlugin({
-            filename: '[name].[hash].css'
-        })
+		new webpack.NamedModulesPlugin(),
+		new HtmlWebpackPlugin()
 	]
 };
